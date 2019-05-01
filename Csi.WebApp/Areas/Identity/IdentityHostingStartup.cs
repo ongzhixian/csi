46176356207f6ef88366cde3a82d5fb33e921212
zhixian <zhixian@hotmail.com>
2019-05-01 11:43:36 +0800

    add identity pages

---
+++ Csi.WebApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+using System;
+using Csi.WebApp.Areas.Identity.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Csi.WebApp.Areas.Identity.IdentityHostingStartup))]
+namespace Csi.WebApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<IdentityDataContext>(options =>
+                    options.UseMySQL("name=CsiDatabase")
+                );
+
+                services.AddDefaultIdentity<CsiUser>()
+                    .AddEntityFrameworkStores<IdentityDataContext>();
+            });
+        }
+    }
+}
