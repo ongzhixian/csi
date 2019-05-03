068f6d9a485f2f2bcbd793f14d85594be478cdcc
Ong Zhixian <overxianz@gmail.com>
2019-05-03 23:52:12 +0800

    Migrations for Identity and IdentityClaim

---
+++ Csi.WebApp/Migrations/20190503152706_CreateIdentitySchema.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Csi.WebApp.Migrations
+{
+    public partial class CreateIdentitySchema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CsiUsers",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    UserName = table.Column<string>(nullable: true),
+                    NormalizedUserName = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    NormalizedEmail = table.Column<string>(nullable: true),
+                    EmailConfirmed = table.Column<short>(nullable: false),
+                    PasswordHash = table.Column<string>(nullable: true),
+                    SecurityStamp = table.Column<string>(nullable: true),
+                    ConcurrencyStamp = table.Column<string>(nullable: true),
+                    PhoneNumber = table.Column<string>(nullable: true),
+                    PhoneNumberConfirmed = table.Column<short>(nullable: false),
+                    TwoFactorEnabled = table.Column<short>(nullable: false),
+                    LockoutEnd = table.Column<DateTimeOffset>(nullable: true),
+                    LockoutEnabled = table.Column<short>(nullable: false),
+                    AccessFailedCount = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CsiUsers", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CsiUsers");
+        }
+    }
+}
---
+++ Csi.WebApp/Migrations/20190503154837_CreateIdentityClaimSchema.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Csi.WebApp.Migrations
+{
+    public partial class CreateIdentityClaimSchema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<short>(
+                name: "TwoFactorEnabled",
+                table: "CsiUsers",
+                nullable: false,
+                oldClrType: typeof(short));
+
+            migrationBuilder.AlterColumn<short>(
+                name: "PhoneNumberConfirmed",
+                table: "CsiUsers",
+                nullable: false,
+                oldClrType: typeof(short));
+
+            migrationBuilder.AlterColumn<short>(
+                name: "LockoutEnabled",
+                table: "CsiUsers",
+                nullable: false,
+                oldClrType: typeof(short));
+
+            migrationBuilder.AlterColumn<short>(
+                name: "EmailConfirmed",
+                table: "CsiUsers",
+                nullable: false,
+                oldClrType: typeof(short));
+
+            migrationBuilder.CreateTable(
+                name: "IdentityUserClaims",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:AutoIncrement", true),
+                    UserId = table.Column<string>(nullable: true),
+                    ClaimType = table.Column<string>(nullable: true),
+                    ClaimValue = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IdentityUserClaims", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "IdentityUserClaims");
+
+            migrationBuilder.AlterColumn<short>(
+                name: "TwoFactorEnabled",
+                table: "CsiUsers",
+                nullable: false,
+                oldClrType: typeof(short));
+
+            migrationBuilder.AlterColumn<short>(
+                name: "PhoneNumberConfirmed",
+                table: "CsiUsers",
+                nullable: false,
+                oldClrType: typeof(short));
+
+            migrationBuilder.AlterColumn<short>(
+                name: "LockoutEnabled",
+                table: "CsiUsers",
+                nullable: false,
+                oldClrType: typeof(short));
+
+            migrationBuilder.AlterColumn<short>(
+                name: "EmailConfirmed",
+                table: "CsiUsers",
+                nullable: false,
+                oldClrType: typeof(short));
+        }
+    }
+}
