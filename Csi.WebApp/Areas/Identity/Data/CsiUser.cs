797c6bfb6b819ee15149de50bbcbe0e0dc6e3732
Ong Zhixian <overxianz@gmail.com>
2019-05-04 00:16:55 +0800

    Updated CsiUser to correct data type conversions used by MySql EF provider

--- Csi.WebApp/Areas/Identity/Data/CsiUser.cs
+++ Csi.WebApp/Areas/Identity/Data/CsiUser.cs
@@ -3,11 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Csi.WebApp.Data
 {
     // Add profile data for application users by adding properties to the CsiUser class
     public class CsiUser : IdentityUser
     {
+        [Column(TypeName = "bit(1)")]
+        public override bool EmailConfirmed { get; set; }
+
+        [Column(TypeName = "bit(1)")]
+        public override bool PhoneNumberConfirmed { get; set; }
+
+        [Column(TypeName = "bit(1)")]
+        public override bool TwoFactorEnabled { get; set; }
+
+        [Column(TypeName = "bit(1)")]
+        public override bool LockoutEnabled { get; set; }
+
     }
 }
