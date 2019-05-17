bc9c0698dfa23dc0842032171597cb24096c1cd4
zhixian <zhixian@hotmail.com>
2019-05-17 21:30:47 +0800

    Add result

---
+++ Csi.Data/Result.cs
@@ -0,0 +1,9 @@
+namespace Csi.Data
+{
+    public class Result
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public Exception Exception { get; set; }
+    }
+}
