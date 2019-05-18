eed9f4fd3b38417bd5485f0e83f0caa9f28ec6f2
zhixian <zhixian@hotmail.com>
2019-05-18 22:49:38 +0800

    defining RegistrationServices

---
+++ Csi.Services/RegistrationServices.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Csi.Services
+{
+    public interface IRegistrationServices
+    {
+        bool RegisterProject(string name);
+    }
+
+    public class RegistrationServices : IRegistrationServices
+    {
+        public bool RegisterProject(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
