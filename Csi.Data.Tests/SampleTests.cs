51d6cb87be57854688ec2c1596bc5f02f648702e
zhixian <zhixian@hotmail.com>
2019-05-18 12:22:44 +0800

    Add code showing how to read config settings in unit tests

--- Csi.Data.Tests/SampleTests.cs
+++ Csi.Data.Tests/SampleTests.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.VisualStudio.TestTools.UnitTesting.Logging;
 
@@ -6,6 +9,27 @@ namespace Csi.Data.Tests
     [TestClass]
     public class SampleTests
     {
+        static IConfiguration configuration = null;
+
+        [ClassInitialize]
+        public static void Init(TestContext context)
+        {
+            configuration = new ConfigurationBuilder()
+                .AddJsonFile("runtime-settings.json", optional: true, reloadOnChange: true)
+                .Build();
+
+            Logger.LogMessage("[{0}] configuration keys found.",
+                configuration.AsEnumerable().Count()
+            );
+            foreach (KeyValuePair<string, string> kvp in configuration.AsEnumerable())
+            {
+                Logger.LogMessage("Key:[{0}], Value:[{1}]", kvp.Key, kvp.Value);
+            }
+            
+            Logger.LogMessage("Value of dummy_clientId1 is [{0}]", configuration["dummy_clientId1"]);
+            Logger.LogMessage("CsiDatabase connectionString is: [{0}]", configuration.GetConnectionString("CsiDatabase"));
+        }
+
         [TestMethod]
         public void OutputTest()
         {
