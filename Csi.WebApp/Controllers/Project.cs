a9b00b1f17afe81474ccf4312ab4ba78003b6f00
zhixian <zhixian@hotmail.com>
2019-05-26 21:51:47 +0800

    change namespace and move file to Controllers folder

--- Csi.WebApp/Controllers/Project.cs
+++ Csi.WebApp/Controllers/Project.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Csi.Data;
 using Csi.WebApp.Data;
 
-namespace Csi.WebApp
+namespace Csi.WebApp.Controllers
 {
     public class Project : Controller
     {
