c3060e055049556b83107863fe09387b73008576
zhixian <zhixian@hotmail.com>
2019-06-01 05:13:36 +0800

    Change namespace to include Area

--- Csi.WebApp/Areas/Sample/Controllers/HomeController.cs
+++ Csi.WebApp/Areas/Sample/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Csi.WebApp.Sample
+namespace Csi.WebApp.Areas.Sample
 {
     [Area("Sample")]
     public class HomeController : Controller
