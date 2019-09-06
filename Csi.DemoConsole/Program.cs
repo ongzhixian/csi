4c99657b3781aef23193f7065d8d43e24cca0f46
zhixian <zhixian@hotmail.com>
2019-09-07 00:03:51 +0800

    Init DemoConsole

--- Csi.DemoConsole/Program.cs
+++ Csi.DemoConsole/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Core;
 
 namespace Csi.DemoConsole
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("[PROGRAM START]");
+
+            Console.WriteLine(args.Length);
+
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+            
+            Logger log = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+            Log.Logger = log; // Wired it back to the static Log instance
+
+            Log.Verbose(    "Test Verbose     message");
+            Log.Debug(      "Test Debug       message");
+            Log.Information("Test Information message");
+            Log.Warning(    "Test Warning     message");
+            Log.Error(      "Test Error       message");
+            Log.Fatal(      "Test Fatal       message");
+
+            
+            Console.WriteLine("[PROGRAM END!!]");
         }
     }
 }
---
+++ Csi.DemoConsole/_docs/01-packages.md
@@ -0,0 +1,10 @@
+# Packages
+==========
+
+dotnet add package Serilog
+dotnet add package Serilog.Sinks.Console
+dotnet add package Serilog.Sinks.File
+dotnet add package Serilog.Settings.Configuration
+
+dotnet add package Microsoft.Extensions.Configuration
+dotnet add package Microsoft.Extensions.Configuration.Json
