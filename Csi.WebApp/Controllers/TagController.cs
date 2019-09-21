67e29a8a85330c1f33b76374c551ff0bc222badf
zhixian <zhixian@hotmail.com>
2019-09-21 10:35:41 +0800

    Add Tag

---
+++ Csi.WebApp/Controllers/TagController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Csi.WebApp.Models;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Authorization;
+using Csi.WebApp.Data;
+
+namespace Csi.WebApp.Controllers
+{
+    public class TagController : Controller
+    {
+        private readonly ILogger logger;
+
+        private readonly CsiSQLiteDbContext db;
+
+        public TagController(ILogger<TagController> logger, CsiSQLiteDbContext context)
+        {
+            this.logger = logger;
+            this.db = context;
+        }
+
+        public IActionResult Index()
+        {
+            this.logger.LogInformation("In -- TagController-Index");
+
+            return View();
+        }
+
+    }
+}
