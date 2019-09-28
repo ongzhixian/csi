7761a0bbb81092b5cde9b0b6426c8510ad93a2a7
zhixian <zhixian@hotmail.com>
2019-09-28 08:09:28 +0800

    Add research controller

---
+++ Csi.WebApp/Controllers/ResearchController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Csi.WebApp.Models;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Authorization;
+using Csi.WebApp.Data;
+
+namespace Csi.WebApp.Controllers
+{
+    public class ResearchController : Controller
+    {
+        private readonly ILogger logger;
+
+        private readonly CsiSQLiteDbContext db;
+
+        public ResearchController(ILogger<ResearchController> logger, CsiSQLiteDbContext context)
+        {
+            this.logger = logger;
+            this.db = context;
+        }
+
+        public IActionResult Index()
+        {
+            this.logger.LogInformation("In -- ResearchController-Index");
+
+            return View();
+        }
+
+    }
+}
