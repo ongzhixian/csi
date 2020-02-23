14dfc939347dfa08f9abe78ca9c875d2c5f62fcc
Ong Zhixian <overxianz@gmail.com>
2020-02-23 09:39:38 +0800

    UPStw

---
+++ Csi.LookMeUp/Controllers/AccountController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Csi.LookMeUp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+
+namespace Csi.LookMeUp.Controllers
+{
+    public class HomeController : Controller
+    {
+        ILogger<HomeController> log = null;
+
+        public HomeController(ILogger<HomeController> log)
+        {
+            this.log = log;
+        }
+        public IActionResult Index()
+        {
+            log.LogInformation("Index page says hello");
+
+            log.LogInformation(User.Identity.GetType().ToString());
+
+            ClaimsIdentityUser n = new ClaimsIdentityUser(User.Identity);
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [Route("sign-out")]
+        public async Task<IActionResult> OnPostLogoutAsync()
+        {
+            await HttpContext.SignOutAsync();
+            return Index();
+        }
+    }
+}
